10c4e475ac9974608aab151ef77beeb2ec7f5a34
Karin Ohman <kaohma@gmail.com>
2019-01-06 16:53:30 -0700

    Update let to const for mock data: git checkout master git merge testing

--- src/test/Step1.test.js
+++ src/test/Step1.test.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import Step1 from '../Step1';
 import { shallow } from 'enzyme';
 
-let incrementStepMock = jest.fn();
-let questionMock = 'You are given the following array of your friends ages: [18, 29, 56, 5, 77, 1]. You have a wide variety or friends! All your friends have celebrated their birthdays this year so you want to return an updated array of ages that reflects them all aging by one year.'
+const incrementStepMock = jest.fn();
+const questionMock = 'You are given the following array of your friends ages: [18, 29, 56, 5, 77, 1]. You have a wide variety or friends! All your friends have celebrated their birthdays this year so you want to return an updated array of ages that reflects them all aging by one year.'
 let currentStepMock = 1
 
 describe('Step1', () => {
