3e1aa02b7090516cf9d56a5eab77869d98e6633d
Karin Ohman <kaohma@gmail.com>
2019-01-08 15:03:03 -0700

    Updates

---
+++ precache-manifest.12da962d320bf0b5d1e13a2cd1f97e4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f593a92e9480d0d7f1ef",
+    "url": "/js-proto-fun-time/static/js/runtime~main.f593a92e.js"
+  },
+  {
+    "revision": "d89f52730e110a3eee40",
+    "url": "/js-proto-fun-time/static/js/main.d89f5273.chunk.js"
+  },
+  {
+    "revision": "46e271669118b1cce399",
+    "url": "/js-proto-fun-time/static/js/1.46e27166.chunk.js"
+  },
+  {
+    "revision": "d89f52730e110a3eee40",
+    "url": "/js-proto-fun-time/static/css/main.6e65df3e.chunk.css"
+  },
+  {
+    "revision": "46e271669118b1cce399",
+    "url": "/js-proto-fun-time/static/css/1.52657a4e.chunk.css"
+  },
+  {
+    "revision": "3d8c48a154c1bc1773de8ddf6f8cddf3",
+    "url": "/js-proto-fun-time/index.html"
+  }
+];
